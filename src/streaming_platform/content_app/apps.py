1148bacccc6bb873d9eadc036ebe443ad16c1841
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-11-01 11:27:52 -0300

    foi

--- src/streaming_platform/content_app/apps.py
+++ src/streaming_platform/content_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class ContentAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'content_app'
